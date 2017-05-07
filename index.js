46a867fe4fefb92688ed3ba844235be486ab7e23
xinye.li <xinye.li@try4.org>
2017-05-07 20:36:16 +0800

    add path map to avoid mutilple ref.

--- index.js
+++ index.js
@@ -6,98 +6,105 @@ var vpath = require('./v-path');
 var src = '';
 var dist = '';
 var delimiter = '';
+var pathMap = {};
 
-var renamePath = function(realPath, hashRealPath) {
-  fs.rename(realPath, hashRealPath, function(err) {
-      if (err) {
-        throw err;
-      }
+var renamePath = function (realPath, hashRealPath) {
+  fs.rename(realPath, hashRealPath, function (err) {
+    if (err) {
+      throw err;
+    }
   });
 };
-var processPath = function(pth, fn) {
-    var realPath = vpath.realPath(pth, dist, delimiter);
-    var hash = vhash(fs.readFileSync(realPath));
-    var hashPath = vpath.hashPath(pth, hash);
-    var hashRealPath = vpath.hashPath(realPath, hash);
 
-    fn && fn(hashPath, realPath, hashRealPath);
+var processPath = function (pth, fn) {
+  var realPath = vpath.realPath(pth, dist, delimiter);
+  var hash = vhash(fs.readFileSync(realPath));
+  var hashPath = vpath.hashPath(pth, hash);
+  var hashRealPath = vpath.hashPath(realPath, hash);
+
+  fn && fn(hashPath, realPath, hashRealPath);
 };
 
-var getHtmlPathList = function() {
-    var files = fs.readdirSync(src);
-    var htmlPathList = [];
+var getHtmlPathList = function () {
+  var files = fs.readdirSync(src);
+  var htmlPathList = [];
 
-    files.forEach(function(file) {
-        if (path.extname(file) === '.html') {
-            var filePath = path.join(src, file);
+  files.forEach(function (file) {
+    if (path.extname(file) === '.html') {
+      var filePath = path.join(src, file);
 
-            htmlPathList.push(filePath);
-        }
-    });
+      htmlPathList.push(filePath);
+    }
+  });
 
-    return htmlPathList;
+  return htmlPathList;
 };
 
-var getStaticPathList = function(html) {
-    var matches = html.match(/<(link|script).+?>/g);
-    var staticPathList = [];
+var getStaticPathList = function (html) {
+  var matches = html.match(/<(link|script).+?>/g);
+  var staticPathList = [];
 
-    matches.forEach(function(matched) {
-        if (matched.indexOf('href') > 0 || matched.indexOf('src') > 0) {
-            matched = matched.replace(/.+(href|src)="(.+)"\s*>/g, '$2');
-            matched = matched.replace(/\?.+$/g, '');
+  matches.forEach(function (matched) {
+    if (matched.indexOf('href') > 0 || matched.indexOf('src') > 0) {
+      matched = matched.replace(/.+(href|src)="(.+)"\s*>/g, '$2');
+      matched = matched.replace(/\?.+$/g, '');
 
-            var ext = path.extname(matched);
+      var ext = path.extname(matched);
 
-            if (matched.indexOf(delimiter) !== -1 && matched.indexOf('/lib/') === -1  && (ext === '.js' || ext === '.css')) {//not match /js/lib/* or /css/lib/*
-                staticPathList.push(matched);
-            }
-        }
-    });
+      if (matched.indexOf(delimiter) !== -1 && matched.indexOf('/lib/') === -1 && (ext === '.js' || ext === '.css')) {//not match /js/lib/* or /css/lib/*
+        staticPathList.push(matched);
+      }
+    }
+  });
 
-    return staticPathList;
+  return staticPathList;
 };
 
-var processStaticPath = function(htmlPath) {
-    fs.readFile(htmlPath, function(readErr, data) {
-        if (readErr) {
-            throw readErr;
-        }
+var processStaticPath = function (htmlPath) {
+  fs.readFile(htmlPath, function (readErr, data) {
+    if (readErr) {
+      throw readErr;
+    }
 
-        var html = data.toString();
-        var staticPathList = getStaticPathList(html);
+    var html = data.toString();
+    var staticPathList = getStaticPathList(html);
 
-        staticPathList.forEach(function(staticPath) {
-            processPath(staticPath, function(hashPath, realPath, hashRealPath) {
-                hashPath = hashPath.replace(/\\/g, '/');
-                html = html.replace(staticPath, hashPath);
-                renamePath(realPath, hashRealPath);
-            });
+    staticPathList.forEach(function (staticPath) {
+      if (pathMap[staticPath]) {
+        html = html.replace(staticPath, pathMap[staticPath]);
+      } else {
+        processPath(staticPath, function (hashPath, realPath, hashRealPath) {
+          hashPath = hashPath.replace(/\\/g, '/');
+          html = html.replace(staticPath, hashPath);
+          renamePath(realPath, hashRealPath);
+          pathMap[staticPath] = hashPath;
         });
-
-        fs.writeFile(htmlPath, html, function(writeErr) {
-            if (writeErr) {
-                throw writeErr;
-            }
-        });
+      }
     });
+
+    fs.writeFile(htmlPath, html, function (writeErr) {
+      if (writeErr) {
+        throw writeErr;
+      }
+    });
+  });
 };
 
 module.exports = function (argv) {
-    src = argv.src || '';//where to read .html
-    dist = argv.dist || '';//where to read/write .js/.css
-    delimiter = argv.delimiter || '';
+  src = argv.src || '';//where to read .html
+  dist = argv.dist || '';//where to read/write .js/.css
+  delimiter = argv.delimiter || '';
 
-    if (src === '') {
-        throw new Error('`src` required');
-    }
-    if (dist === '') {
-        throw new Error('`dist` required');
-    }
+  if (src === '') {
+    throw new Error('`src` required');
+  }
+  if (dist === '') {
+    throw new Error('`dist` required');
+  }
 
-    var htmlPathList = getHtmlPathList();
+  var htmlPathList = getHtmlPathList();
 
-    htmlPathList.forEach(function(htmlPath) {
-        processStaticPath(htmlPath);
-    });
+  htmlPathList.forEach(function (htmlPath) {
+    processStaticPath(htmlPath);
+  });
 };
--- v-path.js
+++ v-path.js
@@ -1,33 +1,33 @@
 'use strict';
 var path = require('path');
 
-var hashPath = function(pth, hash) {
+var hashPath = function (pth, hash) {
   var basename = path.posix.basename(pth);
-	var ext = path.extname(pth);
+  var ext = path.extname(pth);
   var newname = path.posix.basename(pth, ext) + '-' + hash + ext;
 
   return pth.replace(basename, newname);
 };
 
-var realPath = function(pth, dist, delimiter) {
-	var subname = pth.replace(delimiter, '');
+var realPath = function (pth, dist, delimiter) {
+  var subname = pth.replace(delimiter, '');
 
-	return path.join(dist, subname);
+  return path.join(dist, subname);
 };
 
 module.exports = {
-	hashPath: function (pth, hash) {
-		if (arguments.length !== 2) {
-			throw new Error('`path` and `hash` required');
-		}
+  hashPath: function (pth, hash) {
+    if (arguments.length !== 2) {
+      throw new Error('`path` and `hash` required');
+    }
 
-		return hashPath(pth, hash);
-	},
-	realPath: function(pth, dist, delimiter) {
-		if (arguments.length !== 3) {
-			throw new Error('`path`, `dist` and `delimiter` required');
-		}
+    return hashPath(pth, hash);
+  },
+  realPath: function (pth, dist, delimiter) {
+    if (arguments.length !== 3) {
+      throw new Error('`path`, `dist` and `delimiter` required');
+    }
 
-		return realPath(pth, dist, delimiter);
-	}
+    return realPath(pth, dist, delimiter);
+  }
 };
